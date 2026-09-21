003e4de8e882d1022f5c4daa2600a8e33a608753
husnain-btc <husnainrce@gmail.com>
2026-09-22 01:25:06 +0500

    Fetch Bitcoin market chart data from API

--- bot.py
+++ bot.py
@@ -1,8 +1,26 @@
 import requests
 
-url = "https://api.coingecko.com/api/v3/ping"
+url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart"
 
-response = requests.get(url, timeout=15)
+params = {
+    "vs_currency": "usd",
+    "days": "2",
+    "interval": "hourly"
+}
+
+response = requests.get(url, params=params, timeout=20)
 
 print("Status:", response.status_code)
-print("Response:", response.text)
+
+response.raise_for_status()
+
+data = response.json()
+
+prices = data["prices"]
+
+print("Number of hourly price points:", len(prices))
+
+print("\nLast 5 hourly prices:")
+
+for timestamp, price in prices[-5:]:
+    print(timestamp, price)
